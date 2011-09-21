2d9200402b91c98e887207ca64c4b6064dd974ae
Phil Thompson <phil@example.com>
2011-09-21 17:14:01 +0100

    Quickly added a search/replace class. Needs tests and polish

--- lib/merrol/lib/file.rb
+++ lib/merrol/lib/file.rb
@@ -2,5 +2,9 @@ class File
   def self.app_relative relative_path
     expand_path join(dirname(__FILE__), '..', '..', '..', relative_path)
   end
+
+  def self.write file, content
+    File.open(file, 'w') {|f| f.write(content) }
+  end
 end
 
